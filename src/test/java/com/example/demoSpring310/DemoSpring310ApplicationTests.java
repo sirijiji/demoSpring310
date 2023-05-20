f44c8ae9d2449d2708a7d294af8ec7ef3c4001e0
sirijiji <sirisak_01@yahoo.fr>
2023-05-20 22:04:20 +0200

    add testcontainers

--- src/test/java/com/example/demoSpring310/DemoSpring310ApplicationTests.java
+++ src/test/java/com/example/demoSpring310/DemoSpring310ApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.demoSpring310;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
-class DemoSpring310ApplicationTests {
+@Configuration
+public class DemoSpring310ApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Bean
+	@RestartScope
+	@ServiceConnection
+	public PostgreSQLContainer postgreSQLContainer(){
+		PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:15.0-alpine");
+		postgreSQLContainer.withInitScript("data.sql");
+		return postgreSQLContainer;
+	}
+
+
+	public static void main(String[] args) {
+
+		SpringApplication
+				.from(DemoSpring310Application::main)
+				.with(DemoSpring310ApplicationTests.class)
+				.run(args);
 	}
 
 }
