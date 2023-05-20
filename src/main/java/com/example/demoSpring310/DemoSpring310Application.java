0546601e5d455c7415f55f1898ff1d9d04457251
sirijiji <sirisak_01@yahoo.fr>
2023-05-20 19:21:32 +0200

    first commit

---
+++ src/main/java/com/example/demoSpring310/DemoSpring310Application.java
@@ -0,0 +1,13 @@
+package com.example.demoSpring310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpring310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpring310Application.class, args);
+	}
+
+}
